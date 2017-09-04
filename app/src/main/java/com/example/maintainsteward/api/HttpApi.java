b79d13d157b7238eae4fdbce22868b2dd3f6eb2b
lxl757266307 <lxl163521@163.com>
2017-09-04 11:12:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/maintainsteward/api/HttpApi.java
@@ -0,0 +1,15 @@
+package com.example.maintainsteward.api;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+
+public interface HttpApi {
+
+
+}
---
+++ app/src/main/java/com/example/maintainsteward/base/Contacts.java
@@ -0,0 +1,16 @@
+package com.example.maintainsteward.base;
+
+/**
+ * Created by Administrator on 2017/8/2.
+ */
+
+public final class Contacts {
+
+    public static final String QINIUYUN = "http://os18w14e3.bkt.clouddn.com/";//七牛云图片域名
+    //public static final String QINIUYUN ="http://oscakuwph.bkt.clouddn.com/";//七牛云图片域名(测试)
+    //public static final String QINIUYUN ="osenv7cs9.bkt.clouddn.com/";//七牛云图片域名(测试)
+
+
+
+
+}
